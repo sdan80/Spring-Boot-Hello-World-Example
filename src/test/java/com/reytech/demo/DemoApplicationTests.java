76fae4384cab66530ca1b71b4d6ef28fd1b69359
JCBR-METAROM <104374278+JCBR-METAROM@users.noreply.github.com>
2022-04-26 11:19:58 +0200

    Update DemoApplicationTests.java

--- src/test/java/com/reytech/demo/DemoApplicationTests.java
+++ src/test/java/com/reytech/demo/DemoApplicationTests.java
@@ -17,7 +17,7 @@ class DemoApplicationTests {
         void groupedAssertions() {
         Personne pers = new Personne("Nassur", "Moumadi");
 
-        assertAll("personne", () -> assertEquals("Nassur", pers.getFirstName()),
+        assertAll("personne", () -> assertEquals("Nassur*", pers.getFirstName()),
                               () -> assertEquals("Moumadi", pers.getLastName()));
     }
 
