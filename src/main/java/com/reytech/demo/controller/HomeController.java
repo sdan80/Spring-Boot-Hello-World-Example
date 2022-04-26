fad5dde6fa5d87c92bb39dc4dfa3d288b56b5a1c
JCBR-METAROM <104374278+JCBR-METAROM@users.noreply.github.com>
2022-04-26 09:46:55 +0200

    Update HomeController.java

--- src/main/java/com/reytech/demo/controller/HomeController.java
+++ src/main/java/com/reytech/demo/controller/HomeController.java
@@ -16,6 +16,6 @@ public class HomeController {
     @GetMapping("/message")
     public String message(Model model) {
         model.addAttribute("message", "This is a custom message");
-        //return "message";
+        return "message";
     }
 }
